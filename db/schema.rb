ed96cf2b3ccd27aae11d01afccce7076a0064a99
Baryskhan Bimat <baryskhan.bimat@ce.sdu.edu.kz>
2017-12-18 21:13:02 +0600

    Create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205180536) do
+ActiveRecord::Schema.define(version: 20171218150744) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "desription"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
