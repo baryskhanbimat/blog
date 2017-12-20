74bb87ee19c237edc1baa12923fb2b773d7f1e6b
Baryskhan Bimat <baryskhan.bimat@ce.sdu.edu.kz>
2017-12-20 22:47:46 +0600

    Add admin functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218174143) do
+ActiveRecord::Schema.define(version: 20171220155133) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171218174143) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "password"
+    t.boolean "admin", default: false
   end
 
 end
