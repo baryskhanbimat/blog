6671364028e8056593bb0d7c078e3b99ddf62f14
Baryskhan Bimat <baryskhan.bimat@ce.sdu.edu.kz>
2017-12-25 23:33:52 +0600

    Add Edit category stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
 
+  resources :categories
+
   get 'about', to: 'pages#about'
 
   get 'signup', to: 'users#new'
@@ -10,8 +12,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
-  resources :categories, except: [:destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
